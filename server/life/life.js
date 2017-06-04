9065c33abec10b2d4e971c8e53a62fca175a6049
Asad Saeeduddin <masaeedu@gmail.com>
2017-06-04 17:50:31 -0400

    wip

--- server/life/life.js
+++ server/life/life.js
@@ -4,6 +4,48 @@
 // expose a function that creates a grid
 import { Point, getCoordinatesOfIndex, getIndexOfCoordinates } from '../../shared/coordinates'
 
+function game(n) {
+    function liveNeighbors({ x, y }, cells) {
+        const neighbors = [
+            [x - 1, y - 1],
+            [x, y - 1],
+            [x + 1, y - 1],
+            [x - 1, y],
+            [x + 1, y],
+            [x - 1, y + 1],
+            [x, y + 1],
+            [x + 1, y + 1]
+        ];
+        return neighbors.filter(pair => {
+            const [x, y] = pair
+            return cells[n * x + y] && inBounds(x, y)
+        }).length;
+    }
+
+    function inBounds(x, y) {
+        const size = n * n;
+        return (x >= 0 && x < size) && (y >= 0 && y < size)
+    }
+
+    function checkForLife(cellState, point, cells) {
+        const neighbors = liveNeighbors(point, cells)
+        if (cellState && (neighbors === 2 || neighbors === 3)) return true
+        if (!cellState && (neighbors === 3)) return true
+        return false
+    }
+
+    return function update(cells) {
+        const dirty = new Set()
+        const cells = Object.freeze(cells)
+        for (var i = 0; i < n * n; i++) {
+            const point = getCoordinates(i)
+            const nextState = checkForLife(cells[i], point, cells)
+            if (cells[i] !== nextState) dirty.add(i)
+        }
+        return dirty
+    }
+}
+
 // the size of the grid
 const n = 100
 let paused = false
@@ -11,41 +53,13 @@ let paused = false
 const getCoordinates = getCoordinatesOfIndex(n)
 const getIndex = getIndexOfCoordinates(n)
 
-const dirty = new Set()
 
 const cells = new Array(n * n).fill(0).map(() => {
     var r = Math.random()
     return Math.round(r)
-});
+})
 
-function liveNeighbors({ x, y }) {
-    const neighbors = [
-        [x - 1, y - 1],
-        [x, y - 1],
-        [x + 1, y - 1],
-        [x - 1, y],
-        [x + 1, y],
-        [x - 1, y + 1],
-        [x, y + 1],
-        [x + 1, y + 1]
-    ];
-    return neighbors.filter(pair => {
-        const [x, y] = pair
-        return cells[n * x + y] && inBounds(x, y)
-    }).length;
-}
 
-function inBounds(x, y) {
-    const size = n * n;
-    return (x >= 0 && x < size) && (y >= 0 && y < size)
-}
-
-function checkForLife(cellState, point) {
-    const neighbors = liveNeighbors(point)
-    if (cellState && (neighbors === 2 || neighbors === 3)) return true
-    if (!cellState && (neighbors === 3)) return true
-    return false
-}
 
 export function evolve() {
     if (!paused) {
@@ -77,5 +91,5 @@ export function togglePause() {
 export function applyInteraction(interactionEvent) {
     console.log(interactionEvent)
     const event = JSON.parse(interactionEvent)
-    if(cells[event.index] === event.erasing) dirty.add(event.index)
+    if (cells[event.index] === event.erasing) dirty.add(event.index)
 }
